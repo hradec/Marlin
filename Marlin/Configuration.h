74471ea9fe2dc91e57967389440e1d3e085b07dd
hradec <pi@rpi.local>
2014-12-16 06:49:29 +0000

    fixed probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -406,7 +406,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 //X:14.10 Y:20.00 Z:1.90
 
   #define X_PROBE_OFFSET_FROM_EXTRUDER 14.1
-  #define Y_PROBE_OFFSET_FROM_EXTRUDER 20
+  #define Y_PROBE_OFFSET_FROM_EXTRUDER -20
   #define Z_PROBE_OFFSET_FROM_EXTRUDER -2
 
   #define Z_RAISE_BEFORE_HOMING 50       // (in mm) Raise Z before homing (G28) for Probe Clearance.
