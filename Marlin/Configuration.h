ff450f1a5c38a3eec29805b2ec90f7aa92d17d35
hradec <me@hradec.com>
2020-04-27 00:16:07 -0700

    fixed bed leveling failing by increasing Z_CLEARANCE_BETWEEN_PROBES and Z_CLEARANCE_MULTI_PROBE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1037,9 +1037,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
  * Example: `M851 Z-5` with a CLEARANCE of 4  =>  9mm from bed to nozzle.
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
-#define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
-#define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
+#define Z_CLEARANCE_DEPLOY_PROBE   15 // Z Clearance for Deploy/Stow
+#define Z_CLEARANCE_BETWEEN_PROBES 10 // Z Clearance between probe points
+#define Z_CLEARANCE_MULTI_PROBE    10 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -20 // Farthest distance below the trigger-point to go before stopping
@@ -1137,7 +1137,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // The size of the print bed
 #define X_BED_SIZE 200
-#define Y_BED_SIZE 400
+#define Y_BED_SIZE 350
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
