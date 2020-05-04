2a7c4808d42b79c5dae97c5a500fca0c71a68bc8
hradec <me@hradec.com>
2020-05-04 13:50:15 -0700

    added ffmpeg wrapper and youtube_upload.sh script. Added old pins.h backup as well.

---
+++ youtube_upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#	--title="Timelapse: {gcode}" --description="Timelapse of {gcode}, printed and recorded via OctoPrint" --category="Science &
+#            Technology" --tags=OctoPrint "{movie}" ; rm -rf "{movie}"
+
+mov=$(echo "$@" | awk '{print $(NF)}')
+
+if [ "$(echo $mov | grep fail)" == "" ] ; then
+	echo /usr/local/bin/youtube-upload "$@" >> ~/youtube.log 2>&1
+	/usr/local/bin/youtube-upload "$@" >> ~/youtube.log 2>&1 && rm -rf "$movie" >> ~/youtube.log
+
+fi
